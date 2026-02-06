326d95d5a3bbe423fee67864b5b668bdaed1dcd4
LogPoseLabs <bshashikumar.dev@gmail.com>
2026-02-06 17:47:25 +0530

    Bump version to 1.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "com.oreki.stumpd"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.8"
+        versionCode = 11
+        versionName = "1.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
